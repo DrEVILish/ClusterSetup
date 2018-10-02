339ebc64af75661c42ad7281bfb2a2d0f4f39544
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-02 19:04:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ apt update && apt upgrade -y
 
 apt install -y \
   automake autoconf apt-transport-https \
-  ca-certificates curl \
+  ca-certificates cpufrequtils curl \
   git golang-go \
   libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev \
   pkg-config \
-  software-properties-common screen \
+  software-properties-common screen sysfsutils \
   xfsprogs 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -19,3 +19,17 @@ apt update
 apt install -y docker-ce glusterd2-* glusterfs-* libntirpc*
 apt upgrade -y
 apt autoremove -y
+
+touch /etc/default/cpufrequtils
+echo 'ENABLE="true"' >> /etc/default/cpufrequtils 
+echo 'GOVERNOR="performance"' >> /etc/default/cpufrequtils
+echo 'MAX_SPEED=1900000' >> /etc/default/cpufrequtils
+echo 'MIN_SPEED=1900000' >> /etc/default/cpufrequtils
+
+echo "devices/platform/11800000.mali\:/devfreq/11800000.mali\:/governor = powersave" >> /etc/sysfs.conf
+service sysfsutils start
+fallocate -l 1G /var/swapfile
+chmod 600 /var/swapfile
+mkswap /var/swapfile
+swapon /var/swapfile
+echo "/var/swapfile    none    swap    sw    0    0" >> /etc/fstab
