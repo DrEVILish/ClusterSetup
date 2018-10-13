e82dc49f7018cb4ef6c7b40876cb3272b519775e
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-13 09:00:30 +0100

    Update installOdroid.sh

--- installOdroid.sh
+++ installOdroid.sh
@@ -1,3 +1,9 @@
+echo "Enter hostname:"
+read hostname
+hostnamectl set-hostname $hostname
+
+echo "Please set a Static IP Address using the Armbian Config"
+
 apt update && apt upgrade -y
 curl -sSL https://get.docker.com/ | sh
 
