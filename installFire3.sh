b4f6031fc54fdbd781c1682ef3addef2648d84e9
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-13 08:56:06 +0100

    Update installFire3.sh
    Install Gluster Native Client

--- installFire3.sh
+++ installFire3.sh
@@ -1,2 +1,10 @@
 apt update && apt upgrade -y
 curl -sSL https://get.docker.com/ | sh
+
+wget -O - https://download.gluster.org/pub/gluster/glusterfs/4.1/rsa.pub | apt-key add -
+DEBID=$(grep 'VERSION_ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+DEBVER=$(grep 'VERSION=' /etc/os-release | grep -Eo '[a-z]+')
+DEBARCH=$(dpkg --print-architecture)
+echo deb https://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/${DEBID}/${DEBARCH}/apt ${DEBVER} main > /etc/apt/sources.list.d/gluster.list
+apt-get update
+apt-get install glusterfs-client
