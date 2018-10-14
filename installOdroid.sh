eb75d1932b4156b333374a80e9a2969f91909ae8
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-14 03:05:56 +0100

    Update installOdroid.sh

--- installOdroid.sh
+++ installOdroid.sh
@@ -7,6 +7,14 @@ echo "Please set a Static IP Address using the Armbian Config"
 apt update && apt upgrade -y
 curl -sSL https://get.docker.com/ | sh
 
+wget -O - https://download.gluster.org/pub/gluster/glusterfs/4.1/rsa.pub | apt-key add -
+DEBID=$(grep 'VERSION_ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+DEBVER=$(grep 'VERSION=' /etc/os-release | grep -Eo '[a-z]+')
+DEBARCH=$(dpkg --print-architecture)
+echo deb https://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/${DEBID}/${DEBARCH}/apt ${DEBVER} main > /etc/apt/sources.list.d/gluster.list
+apt-get update
+apt-get install glusterfs-server
+
 touch /etc/default/cpufrequtils
 echo 'ENABLE="true"' >> /etc/default/cpufrequtils 
 echo 'GOVERNOR="performance"' >> /etc/default/cpufrequtils
@@ -20,7 +28,3 @@ chmod 600 /var/swapfile
 mkswap /var/swapfile
 swapon /var/swapfile
 echo "/var/swapfile    none    swap    sw    0    0" >> /etc/fstab
-
-mkdir -p /etc/glusterfs
-mkdir -p /var/lib/glusterd
-mkdir -p /var/log/glusterfs
