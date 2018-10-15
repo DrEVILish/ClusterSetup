2c0bc0173a48f1a3338c6beea476f9e8eec9b9f2
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-15 05:07:17 +0100

    Update installUpboard.sh

--- installUpboard.sh
+++ installUpboard.sh
@@ -7,6 +7,12 @@ echo "Please set a Static IP Address using the Armbian Config"
 apt update && apt upgrade -y
 curl -sSL https://get.docker.com/ | sh
 
+# Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+
 apt install software-properties-common
 add-apt-repository ppa:gluster/glusterfs-4.1
 apt update
