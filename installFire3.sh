b58eb2849864eaa62f037a4f12ca949b9e46ba3e
Ryan Freeman <DrEVILish@users.noreply.github.com>
2018-10-15 05:06:26 +0100

    Update installFire3.sh

--- installFire3.sh
+++ installFire3.sh
@@ -4,6 +4,10 @@ hostnamectl set-hostname $hostname
 
 echo "Please set a Static IP Address using the Armbian Config"
 
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
 apt update && apt upgrade -y
 curl -sSL https://get.docker.com/ | sh
 
@@ -13,4 +17,4 @@ DEBVER=$(grep 'VERSION=' /etc/os-release | grep -Eo '[a-z]+')
 DEBARCH=$(dpkg --print-architecture)
 echo deb https://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/${DEBID}/${DEBARCH}/apt ${DEBVER} main > /etc/apt/sources.list.d/gluster.list
 apt-get update
-apt-get install glusterfs-client
+apt-get install glusterfs-client -y
